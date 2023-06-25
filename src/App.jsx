96abfe5dcc1b3d57448cbc5e3eadd366d2bc2daf
takashiono2 <takashiono710@gmail.com>
2023-06-25 14:03:20 +0900

    section5_57

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
-
+import { useState } from 'react';
+import uuid from 'react-uuid'
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+    const newNotes = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しい内容",
+      modDate: Date.now()
+    }
+    setNotes([...notes, newNotes]);
+  }
+
+  const onDeleteNote = (id) =>{
+    const filterNotes = notes.filter((note)=>note.id!== id);
+    setNotes(filterNotes);
+  }
+
   return(
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote}/>
       <Main />
     </div>
   )
